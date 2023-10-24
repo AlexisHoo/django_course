d423eb980c39ea07580d0c9a8de6888ae5bd49a6
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-10-24 15:06:17 +0000

    MVT with base.htlm and style

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
 
-def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+def Hello(request):
+    bands = Band.objects.all()
+    return render(request, 'listings/hello.html', {'bands': bands})
+
+    
+def about(request):
+    return HttpResponse('<h1>About Us !</h1>')
