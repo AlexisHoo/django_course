d7262072250dae8e100db17ef97ee99634a15fbf
AlexisHoo <148786652+AlexisHoo@users.noreply.github.com>
2023-10-25 14:41:10 +0000

    complete band object

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(default = '',max_length=1000)
+    year_formed = models.fields.IntegerField(default = 2000,
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    genre = models.fields.CharField(default = 'HH',choices=Genre.choices, max_length=5)
+
+    
